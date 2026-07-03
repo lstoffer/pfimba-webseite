2f113b139fa2cfeb0512e6643f27116396479b6b
Luc Stoffer <luc.stoffer@outlook.com>
2026-07-03 21:15:15 +0200

    Some header and navigation changes

--- site/snippets/navigation.php
+++ site/snippets/navigation.php
@@ -1,4 +1,9 @@
 <nav class="navigation" id="site-navigation">
+<div class="navigation-logo">
+    <a href="/">
+        <img src="<?= url('/assets/images/logo/logo_rgb.png') ?>" alt="logo">
+    </a>
+</div>
 <ul>
 <?php foreach($site->children()->listed() as $item): ?>
     <?php $hasChildren = $item->children()->listed()->count() > 0 ?>
