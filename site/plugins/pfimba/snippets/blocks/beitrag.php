e233d982e15323edff933bc64c8c6bd216241879
Luc Stoffer <luc.stoffer@outlook.com>
2026-08-02 22:41:50 +0200

    links for beiträge

--- site/plugins/pfimba/snippets/blocks/beitrag.php
+++ site/plugins/pfimba/snippets/blocks/beitrag.php
@@ -5,9 +5,16 @@
   $bildPosition = $block->bild_position()->value() ?? 'top-center';
   $bildGroesse  = $block->bild_groesse()->value() ?? 'normal';
   $textAlign    = $block->text_ausrichtung()->value() ?? 'links';
+
+  $link           = $block->link()->toLinkUrl();
+  $linkIsExternal = $link && str_starts_with($link, 'http') && !str_starts_with($link, site()->url());
+  $tag            = $link ? 'a' : 'article';
 ?>
 
-<article class="beitrag beitrag--<?= esc($bildPosition) ?> beitrag--img-<?= esc($bildGroesse) ?> beitrag--text-<?= esc($textAlign) ?>">
+<<?= $tag ?>
+  class="beitrag beitrag--<?= esc($bildPosition) ?> beitrag--img-<?= esc($bildGroesse) ?> beitrag--text-<?= esc($textAlign) ?>"
+  <?php if ($link): ?>href="<?= esc($link) ?>"<?php if ($linkIsExternal): ?> target="_blank" rel="noopener"<?php endif; ?><?php endif; ?>
+>
 
   <?php if ($titel->isNotEmpty()): ?>
     <h3 class="beitrag-titel">
@@ -34,4 +41,4 @@
 
   </div>
 
-</article>
+</<?= $tag ?>>
